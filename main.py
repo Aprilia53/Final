771a415a700ffb2866be4cd64f7b64426d8b72c6
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-10-22 03:28:11 +0000

    made the first part of the game

---
+++ main.py
@@ -0,0 +1,14 @@
+# Group members include: Aprilia Galata, Tara Tabla, Lola Watson, and Franco Barboza 
+
+# setting up the story with simple variables and print statements = everyone
+print("Welcome to our short adventure game!")
+print("what is your name?")
+name = input() 
+print("Nice to meet you, " + name + "!") 
+print("Your on a quest to climb the tallest mountain in your small town. ")
+print("No one had ever done it before, so good luck!") 
+answer = input()
+print("alright, lets get started. ")
+print("You wake up early in the morning, and start your treck. You soon realize, you don't have any supplise!")
+print("you walk to a nearby shop that has everything.") 
+
